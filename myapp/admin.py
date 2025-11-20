ec775044135a4e73d8ce5f3753fbcc23b865f7fb
Shandrima K <shandrima.k@accionlabs.com>
2025-11-20 20:46:51 +0530

    Update Django project: edited models and admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,19 +3,46 @@ from django.contrib import admin
 # Register your models here.
 from .models import Student, Mark
 
+admin.site.site_header = "My Student Portal"
+admin.site.index_title = "Welcome to Student Admin Panel"
+
 
 class MarkInline(admin.TabularInline):   
     model = Mark
     extra = 1  
 
+@admin.action(description="Make selected students Active")
+def make_students_active(modeladmin, request, queryset):
+    queryset.update(is_active=True)
+
+@admin.action(description="Make selected students Inactive")
+def make_students_inactive(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "age", "course","gender","is_active")
+    list_display = ("id", "name", "age", "course","gender","joining_date","is_active")
+    list_per_page = 5
     list_filter = ("course", "is_active")
     search_fields = ("name", "course")
+    list_editable = ("course", "is_active")
+    ordering = ("name",)
+    actions = [make_students_active, make_students_inactive]
+
+
     inlines = [MarkInline]
 
+    fieldsets = (
+        ("Personal Info", {"fields": ("name", "age", "gender")}),
+        ("Academic Info", {"fields": ("course","is_active")}),  
+    )
+
     def get_readonly_fields(self, request, obj=None):
+    
         if obj and not obj.is_active:
-            return ("age",)
+            return ("joining_date", "age")  # both are read-only
+    
         return ()
+    
+
--- myapp/models.py
+++ myapp/models.py
@@ -10,11 +10,27 @@ class Student(models.Model):
         ('female', 'Female'),
     ]
 
+    COURSE_CHOICES = [
+        ('bca', 'BCA'),
+        ('computer science', 'COMPUTER SCIENCE'),
+        ('data science', 'DATA SCIENCE'),
+        ('cyber sequrity', 'CYBER SEQURITY'),
+        ('ai', 'AI'),
+        ('btech','BTECH'),
+        ('bcom','BCOM'),
+
+    ]
+
+
     name = models.CharField(max_length=100)
     age = models.IntegerField()
-    course = models.CharField(max_length=100)
+    course = models.CharField(max_length=100, choices=COURSE_CHOICES)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default='male')
     is_active = models.BooleanField(default=True)
+    joining_date = models.DateField(auto_now_add=True) 
+
+    def __str__(self):
+        return self.name
     
 class Mark(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
